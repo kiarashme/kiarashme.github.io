0b796e969f220759de30e8b823e076611f0699e6
Kiarash Adl <kiarasha@kiarasha-macbookpro.roam.corp.google.com>
2015-10-06 14:48:26 -0400

    cleaning up html and js

--- walle/createEve.js
+++ walle/createEve.js
@@ -2,99 +2,165 @@
 
 function createEve() {
 
-//white material for Eve's body
-var eveMat = new THREE.MeshPhongMaterial({ 
-      color: 0xEF0F0FB,
-      ambient: 0xF0F0FB,
-      specular:0xFFFFFF,
-      shininess: 0,
-      side: THREE.DoubleSide
-     
- });
+    //white material for Eve's body
+    var eveMat = new THREE.MeshPhongMaterial({
+        color: 0xEF0F0FB,
+        ambient: 0xF0F0FB,
+        specular: 0xFFFFFF,
+        shininess: 0,
+        side: THREE.DoubleSide
 
-var faceTexture = new THREE.ImageUtils.loadTexture("eveFace.jpg", 
-    new THREE.UVMapping(),
-    function () {
-      console.log("eyeTexture is loaded.");
-      imageLoaded = true;
-      TW.render();
-    });
-// material with the texture of Eve's face 
-  var faceMat = new THREE.MeshPhongMaterial(
-    { color: 0x898989,
-      specular:0xFFFFFF,
-      shininess: 0,
-      map: faceTexture,
-      side: THREE.BackSide
-  
     });
 
-//points that will be used to create a bezier surface for Eve's body
-var evePoints = [ [ [4.5,14,0],  [3.4,7,0], [3,0.5,0], [0,1,0] ],                
-                [ [2.5,14,4.5], [1.5,7,3.4],  [2,0.5,3.2], [0,1,0] ],
-                [ [-2.5,14,4.5], [-1.5,7,3.4],  [-2,0.5,3.2],  [0,1,0] ],
-                [ [-4.5,14,0],  [-3.4,7,0],  [-3,0.5,0],  [0,1,0] ] ];
+    var faceTexture = new THREE.ImageUtils.loadTexture("eveFace.jpg",
+        new THREE.UVMapping(),
+        function() {
+            console.log("eyeTexture is loaded.");
+            imageLoaded = true;
+            TW.render();
+        });
+    // material with the texture of Eve's face 
+    var faceMat = new THREE.MeshPhongMaterial({
+        color: 0x898989,
+        specular: 0xFFFFFF,
+        shininess: 0,
+        map: faceTexture,
+        side: THREE.BackSide
 
-//points that will be used to create a bezier surface for Eve's head
-var headPoints = [ [ [4.5,15,0],  [4.3,16,0], [3.5,20,0], [0,20,0] ],                
-                [ [3,15,4.5], [1,16,4],  [0.4,20,3], [0,20,0] ],
-                [ [-3,15,4.5], [-1,16,4],  [-0.4,20,3],  [0,20,0] ],
-                [ [-4.5,15,0],  [-4.3,16,0],  [-3.5,20,0],  [0,20,0] ] ];
+    });
 
-//points that will be used to create a bezier surface for covering the body with a cap 
-//(cap is needed because without the body looks more like cone)
-var bodyCapPoints = [ [ [4.5,14,0],  [4.4,15,0], [3.5,15.5,0], [0,16,0] ],                
-                    [ [2.5,14,4.5], [1.5,15,4.4],  [2,15.5,3.5], [0,16,0] ],
-                    [ [-2.5,14,4.5], [-1.5,15,4.4],  [-2,15.5,3.5],  [0,16,0] ],
-                    [ [-4.5,14,0],  [-4.4,15,0],  [-3.5,15.5,0],  [0,16,0] ] ];
+    //points that will be used to create a bezier surface for Eve's body
+    var evePoints = [
+        [
+            [4.5, 14, 0],
+            [3.4, 7, 0],
+            [3, 0.5, 0],
+            [0, 1, 0]
+        ],
+        [
+            [2.5, 14, 4.5],
+            [1.5, 7, 3.4],
+            [2, 0.5, 3.2],
+            [0, 1, 0]
+        ],
+        [
+            [-2.5, 14, 4.5],
+            [-1.5, 7, 3.4],
+            [-2, 0.5, 3.2],
+            [0, 1, 0]
+        ],
+        [
+            [-4.5, 14, 0],
+            [-3.4, 7, 0],
+            [-3, 0.5, 0],
+            [0, 1, 0]
+        ]
+    ];
+
+    //points that will be used to create a bezier surface for Eve's head
+    var headPoints = [
+        [
+            [4.5, 15, 0],
+            [4.3, 16, 0],
+            [3.5, 20, 0],
+            [0, 20, 0]
+        ],
+        [
+            [3, 15, 4.5],
+            [1, 16, 4],
+            [0.4, 20, 3],
+            [0, 20, 0]
+        ],
+        [
+            [-3, 15, 4.5],
+            [-1, 16, 4],
+            [-0.4, 20, 3],
+            [0, 20, 0]
+        ],
+        [
+            [-4.5, 15, 0],
+            [-4.3, 16, 0],
+            [-3.5, 20, 0],
+            [0, 20, 0]
+        ]
+    ];
+
+    //points that will be used to create a bezier surface for covering the body with a cap 
+    //(cap is needed because without the body looks more like cone)
+    var bodyCapPoints = [
+        [
+            [4.5, 14, 0],
+            [4.4, 15, 0],
+            [3.5, 15.5, 0],
+            [0, 16, 0]
+        ],
+        [
+            [2.5, 14, 4.5],
+            [1.5, 15, 4.4],
+            [2, 15.5, 3.5],
+            [0, 16, 0]
+        ],
+        [
+            [-2.5, 14, 4.5],
+            [-1.5, 15, 4.4],
+            [-2, 15.5, 3.5],
+            [0, 16, 0]
+        ],
+        [
+            [-4.5, 14, 0],
+            [-4.4, 15, 0],
+            [-3.5, 15.5, 0],
+            [0, 16, 0]
+        ]
+    ];
 
 
-//creating the first half of the body
-var eveHalfGeom = new THREE.BezierSurfaceGeometry( evePoints.reverse(), 30, 30 );
-var halfEve = new THREE.Mesh(eveHalfGeom, eveMat);
-//creating the second half of the body and reflecting it in y axis
-var halfEve2 = halfEve.clone();
-halfEve2.rotation.y= Math.PI;
-var eveBody = new THREE.Object3D();
-eveBody.add(halfEve);
-eveBody.add(halfEve2);
+    //creating the first half of the body
+    var eveHalfGeom = new THREE.BezierSurfaceGeometry(evePoints.reverse(), 30, 30);
+    var halfEve = new THREE.Mesh(eveHalfGeom, eveMat);
+    //creating the second half of the body and reflecting it in y axis
+    var halfEve2 = halfEve.clone();
+    halfEve2.rotation.y = Math.PI;
+    var eveBody = new THREE.Object3D();
+    eveBody.add(halfEve);
+    eveBody.add(halfEve2);
 
-//creating half of the head
-var halfHeadGeom = new THREE.BezierSurfaceGeometry( headPoints.reverse(), 100, 100 );
-var halfHead = new THREE.Mesh(halfHeadGeom, faceMat);
-//creating second half of the head
-var half2Head = new THREE.Mesh(halfHeadGeom, eveMat);
-half2Head.rotation.y= Math.PI;
+    //creating half of the head
+    var halfHeadGeom = new THREE.BezierSurfaceGeometry(headPoints.reverse(), 100, 100);
+    var halfHead = new THREE.Mesh(halfHeadGeom, faceMat);
+    //creating second half of the head
+    var half2Head = new THREE.Mesh(halfHeadGeom, eveMat);
+    half2Head.rotation.y = Math.PI;
 
-//creating the cap for Eve's body
-var bodyCapGeom = new THREE.BezierSurfaceGeometry(bodyCapPoints.reverse(), 30, 30 );
-//creaing the first half of the cap
-var bodyCap1 = new THREE.Mesh(bodyCapGeom, eveMat);
-//creating the second half of the cap and reflecting it in y axis
-var bodyCap2 = bodyCap1.clone();
-bodyCap2.rotation.y = Math.PI;
-var bodyCap = new THREE.Object3D();
-bodyCap.add(bodyCap1);
-bodyCap.add(bodyCap2);
+    //creating the cap for Eve's body
+    var bodyCapGeom = new THREE.BezierSurfaceGeometry(bodyCapPoints.reverse(), 30, 30);
+    //creaing the first half of the cap
+    var bodyCap1 = new THREE.Mesh(bodyCapGeom, eveMat);
+    //creating the second half of the cap and reflecting it in y axis
+    var bodyCap2 = bodyCap1.clone();
+    bodyCap2.rotation.y = Math.PI;
+    var bodyCap = new THREE.Object3D();
+    bodyCap.add(bodyCap1);
+    bodyCap.add(bodyCap2);
 
-eveBody.add(bodyCap);
+    eveBody.add(bodyCap);
 
-//I'm creating Eve's hands by reusing the body geometry and scaling it down to appropriate proportions
-var hand1 = eveBody.clone();
-hand1.scale.set(0.2,0.7, 0.2);
-hand1.position.set(5,3,0);
-hand1.rotation.y = Math.PI/2;
-//creating the second hand by locning the first one
-var hand2 = hand1.clone();
-hand2.position.set(-5,3,0);
-var eve = new THREE.Object3D();
-eve.add(halfHead);
-eve.add(half2Head);
+    //I'm creating Eve's hands by reusing the body geometry and scaling it down to appropriate proportions
+    var hand1 = eveBody.clone();
+    hand1.scale.set(0.2, 0.7, 0.2);
+    hand1.position.set(5, 3, 0);
+    hand1.rotation.y = Math.PI / 2;
+    //creating the second hand by locning the first one
+    var hand2 = hand1.clone();
+    hand2.position.set(-5, 3, 0);
+    var eve = new THREE.Object3D();
+    eve.add(halfHead);
+    eve.add(half2Head);
 
-eve.add(eveBody);
-eve.add(hand1);
-eve.add(hand2);
+    eve.add(eveBody);
+    eve.add(hand1);
+    eve.add(hand2);
 
-return eve;
+    return eve;
 
 }
--- walle/createStem.js
+++ walle/createStem.js
@@ -1,42 +1,65 @@
-
 //This function creates a stem that Wall-E holds and then hands to Eve
 
 function createStem() {
-var greenMat = new THREE.MeshBasicMaterial({color: 0x317A28, // green
-	side: THREE.DoubleSide});
+    var greenMat = new THREE.MeshBasicMaterial({
+        color: 0x317A28, // green
+        side: THREE.DoubleSide
+    });
 
-// points for creating a bezier surface for the leaf
-var leafPoints = [ [ [0,0,0],  [1.5,-1,1], [2,-2,2], [2.5,-0.2,0] ],
-[ [0,0,0], [1.5,-1,0.5],  [2,-1.5,1.5], [2.5,-0.2,0] ],
-[ [0,0,0], [1.5,-1,-0.5],  [2,-1.5,-1.5],  [2.5,-0.2,0] ],
-[ [0,0,0],  [1.5,-1.5,-1],  [2,-2,-2],  [2.5,-0.2,0] ] ];
+    // points for creating a bezier surface for the leaf
+    var leafPoints = [
+        [
+            [0, 0, 0],
+            [1.5, -1, 1],
+            [2, -2, 2],
+            [2.5, -0.2, 0]
+        ],
+        [
+            [0, 0, 0],
+            [1.5, -1, 0.5],
+            [2, -1.5, 1.5],
+            [2.5, -0.2, 0]
+        ],
+        [
+            [0, 0, 0],
+            [1.5, -1, -0.5],
+            [2, -1.5, -1.5],
+            [2.5, -0.2, 0]
+        ],
+        [
+            [0, 0, 0],
+            [1.5, -1.5, -1],
+            [2, -2, -2],
+            [2.5, -0.2, 0]
+        ]
+    ];
 
-//creaging bezier surface
-var leafGeom = new THREE.BezierSurfaceGeometry( leafPoints.reverse(), 30, 30 );
+    //creaging bezier surface
+    var leafGeom = new THREE.BezierSurfaceGeometry(leafPoints.reverse(), 30, 30);
 
-var leaf = new THREE.Mesh(leafGeom, greenMat);
-leaf.rotation.x= Math.PI;
-//making a second leaf using the first one
-var leaf2 = leaf.clone(); 
-leaf.rotation.y=Math.PI;
-leaf.position.set(0,4,0);
-leaf2.position.set(0,6.5,0);
+    var leaf = new THREE.Mesh(leafGeom, greenMat);
+    leaf.rotation.x = Math.PI;
+    //making a second leaf using the first one
+    var leaf2 = leaf.clone();
+    leaf.rotation.y = Math.PI;
+    leaf.position.set(0, 4, 0);
+    leaf2.position.set(0, 6.5, 0);
 
-//points that will be used to create a tube geometry for the stem
-var stemPoints = [];
-  stemPoints.push(new THREE.Vector3(0,0,0));
-  stemPoints.push(new THREE.Vector3(1,3,0));
-  stemPoints.push(new THREE.Vector3(-1,5,0));
-  stemPoints.push(new THREE.Vector3(0.5,7,0));
+    //points that will be used to create a tube geometry for the stem
+    var stemPoints = [];
+    stemPoints.push(new THREE.Vector3(0, 0, 0));
+    stemPoints.push(new THREE.Vector3(1, 3, 0));
+    stemPoints.push(new THREE.Vector3(-1, 5, 0));
+    stemPoints.push(new THREE.Vector3(0.5, 7, 0));
 
-//creating tube geometry
-var stemGeom =  new THREE.TubeGeometry(new THREE.SplineCurve3(stemPoints), 64, 0.1, 10, false);
-var stem = new THREE.Mesh(stemGeom, greenMat);
+    //creating tube geometry
+    var stemGeom = new THREE.TubeGeometry(new THREE.SplineCurve3(stemPoints), 64, 0.1, 10, false);
+    var stem = new THREE.Mesh(stemGeom, greenMat);
 
-var fullStem = new THREE.Object3D();
-fullStem.add(leaf);
-fullStem.add(leaf2);
-fullStem.add(stem);
+    var fullStem = new THREE.Object3D();
+    fullStem.add(leaf);
+    fullStem.add(leaf2);
+    fullStem.add(stem);
 
-return fullStem;
+    return fullStem;
 }
